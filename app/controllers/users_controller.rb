4bdf82fa8f70eb1b05d20e6066da286f7a5d769e
kaho.tsujine <tsuzyshoo@gmail.com>
2019-07-27 08:37:23 +0000

    signup n login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,17 @@ class UsersController < ApplicationController
   
   def index
   end
-
+  
   def new
     @user = User.new
   end
 
   def create
-     @user = User.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to login_path
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
